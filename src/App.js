561ac3fc7cde439bb3f5245e48d2931bfdc91162
AnaKNavarro <esdla_6282@hotmail.com>
2025-01-01 22:30:38 -0600

    feat: Agregado carrusel de imágenes responsivo con 10 slides y autoplay de 2 segundos

--- src/App.js
+++ src/App.js
@@ -1,9 +1,54 @@
 import React from 'react';
 import './App.css';
+import Slider from 'react-slick';
+import "slick-carousel/slick/slick.css";
+import "slick-carousel/slick/slick-theme.css";
 import backgroundImage from './images/fondo_home_1.png'; // Ajusta el nombre del archivo según tu imagen
 import logo from './images/logo.png'; // Asegúrate de añadir tu logo
+import carrusel1 from './images/1carrusel.png';
+import carrusel2 from './images/2carrusel.png';
+import carrusel3 from './images/3carrusel.png';
+import carrusel4 from './images/4carrusel.png';
+import carrusel5 from './images/5carrusel.png';
+import carrusel6 from './images/6carrusel.png';
+import carrusel7 from './images/7carrusel.png';
+import carrusel8 from './images/8carrusel.png';
+import carrusel9 from './images/9carrusel.png';
+import carrusel10 from './images/10carrusel.png';
 
 function App() {
+  const sliderSettings = {
+    dots: true,
+    infinite: true,
+    speed: 500,
+    slidesToShow: 3,
+    slidesToScroll: 1,
+    autoplay: true,
+    autoplaySpeed: 2000,
+    arrows: true,
+    adaptiveHeight: true,
+    centerMode: true,
+    centerPadding: '0px',
+    responsive: [
+      {
+        breakpoint: 1024,
+        settings: {
+          slidesToShow: 2,
+          slidesToScroll: 1,
+          centerMode: true,
+        }
+      },
+      {
+        breakpoint: 600,
+        settings: {
+          slidesToShow: 1,
+          slidesToScroll: 1,
+          centerMode: true,
+        }
+      }
+    ]
+  };
+
   return (
     <div className="App">
       {/* Primera sección - Home */}
@@ -22,6 +67,41 @@ function App() {
               <div className="specialty-tag">Análisis</div>
               <div className="specialty-tag">Visualización</div>
             </div>
+          </div>
+
+          <div className="carousel-container">
+            <Slider {...sliderSettings}>
+              <div className="carousel-slide">
+                <img src={carrusel1} alt="Slide 1" />
+              </div>
+              <div className="carousel-slide">
+                <img src={carrusel2} alt="Slide 2" />
+              </div>
+              <div className="carousel-slide">
+                <img src={carrusel3} alt="Slide 3" />
+              </div>
+              <div className="carousel-slide">
+                <img src={carrusel4} alt="Slide 4" />
+              </div>
+              <div className="carousel-slide">
+                <img src={carrusel5} alt="Slide 5" />
+              </div>
+              <div className="carousel-slide">
+                <img src={carrusel6} alt="Slide 6" />
+              </div>
+              <div className="carousel-slide">
+                <img src={carrusel7} alt="Slide 7" />
+              </div>
+              <div className="carousel-slide">
+                <img src={carrusel8} alt="Slide 8" />
+              </div>
+              <div className="carousel-slide">
+                <img src={carrusel9} alt="Slide 9" />
+              </div>
+              <div className="carousel-slide">
+                <img src={carrusel10} alt="Slide 10" />
+              </div>
+            </Slider>
           </div>
 
           <div className="skills-grid">
